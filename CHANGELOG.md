2e99ef5dd868f411d2a1e15ecb3a6269e7f2f385
Oscar Alfonso Guerrero <oaguerrero.95@gmail.com>
2024-09-19 21:32:10 -0600

    v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v0.0.2
+
+- Added `mod.ts`
+- Updated `deno.json` to jsr standard.
+
 ## v0.0.1
 
 - Added all example code.
---
+++ mod.ts
@@ -0,0 +1,8 @@
+export { default as Atom } from "./components/Atom/index.tsx";
+export * from "./components/Atom/setup.ts";
+export { default as Molecule } from "./components/Molecule/index.tsx";
+export * from "./components/Molecule/setup.ts";
+export * from "./src/enums.ts";
+export * from "./src/utils.ts";
+export * from "./src/types.ts";
+export * from "./plugin.ts";
